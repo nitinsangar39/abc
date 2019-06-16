f8906a8a9b49907cbf6016f0cb6583bb8350e9fc
Nitin <34775302+nitinsangar39@users.noreply.github.com>
2019-06-16 15:26:04 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/home', function(req, res, next){
+  res.render('home');
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about');
+});
+
+router.get('/contect', function(req, res, next){
+  res.render('contect');
+});
+
+module.exports = router;
